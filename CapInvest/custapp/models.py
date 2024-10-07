976b9c97b432ad31a62fc54234b49f58c87f238a
aishwarya-nagtilak08 <aishwaryanagtilak08@gmail.com>
2024-10-07 14:32:52 +0530

    model added

--- CapInvest/custapp/models.py
+++ CapInvest/custapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100, blank=True, null=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    modified_by = models.CharField(max_length=100, blank=True, null=True)
+    
+    class Meta:
+        abstract = True
+
+
+class Customer(BaseModel):
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(unique=False, null=True, blank=True)
+    phone_number = models.CharField(unique=True, max_length=15, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
